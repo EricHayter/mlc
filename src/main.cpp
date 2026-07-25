23f6bffd53f5df7d83d949e8fc5c50e0a1637627
Eric <hayter.eric@gmail.com>
2026-07-25 06:41:03 -0400

    Implement basic main function skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
+#include <argparse/argparse.hpp>
+
+#include <format>
 #include <iostream>
 
-int main(int argc, const char** args) {
-    std::cout << "hello world!\n";
+std::size_t benchmark(std::size_t buffer_size_kb, bool use_huge_pages);
+
+int main(int argc, const char** argv) {
+    argparse::ArgumentParser program("mlc");
+
+    program.add_argument("-H", "--huge")
+        .help("Use huge pages")
+        .flag();
+
+    program.add_argument("--id")
+        .required()
+        .help("CPU id to use")
+        .scan<'i', int>();
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception& err) {
+        std::cerr << err.what() << '\n';
+        std::cerr << program;
+        return 1;
+    }
+
+    for (std::size_t buffer_size_kb = 100; buffer_size_kb < 1024; buffer_size_kb += 100) {
+        std::size_t latency_ns = benchmark(buffer_size_kb, true);
+        std::cout << std::format("{}, {}\n", buffer_size_kb, latency_ns);
+    }
 }
